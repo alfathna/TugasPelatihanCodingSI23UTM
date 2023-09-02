8ccf490d3127a1a54e256809e9837e808fedbe43
alfathna <143860010+alfathna@users.noreply.github.com>
2023-09-02 15:42:14 +0700

    Add files via upload

---
+++ celcius-fahrenheitSI23.py
@@ -0,0 +1,11 @@
+#Celcius to Fahrenheit
+print("Celcius-fahrenheit")
+celcius = int(input("masukkan suhu c "))
+a = 9/5
+b = 32
+hitung = a * celcius + b
+print("rumus celcius-fahrenheit : 9/5 x c x 32")
+print("fahrenheit adalah", hitung)
+
+
+        
